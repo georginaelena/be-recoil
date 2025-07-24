6113d05d8545b792325e641a7d1eedc76fb01843
Georgina Elena Shinta Dewi achti <georgina.elena@ui.ac.id>
2025-07-24 15:13:08 +0700

    edit settings cors

--- BE_ReCoil/settings.py
+++ BE_ReCoil/settings.py
@@ -25,12 +25,16 @@ SECRET_KEY = 'django-insecure-96_r+f46&-_@@v7z1ns5g942z@i+^zv=%#$k*scb1zx6@8q_5f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "spatial-nixie-georginaelena-e92e446b.koyeb.app",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'BE_ReCoil.urls'
 
